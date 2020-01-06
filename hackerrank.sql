19dcb888247b9c60847537e2830980bc9ba651b9
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-05 17:57:46 -0800

    Matches both start and end characters

--- hackerrank.sql
+++ hackerrank.sql
@@ -46,3 +46,12 @@ where city RLIKE '^[aeiou]';
 select distinct city
 from station
 where city rlike '[aeiou]$';
+
+-- Weather Observation Station 8
+--------------------------------
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+select distinct city
+from station
+where city rlike '^[aeiou]'
+and city rlike '[aeiou]$';
