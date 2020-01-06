7def91ac99dd489f36b4417fa5b05cf3d6195e6d
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-05 18:03:41 -0800

    Names that do not end with characters

--- hackerrank.sql
+++ hackerrank.sql
@@ -63,3 +63,11 @@ and city rlike '[aeiou]$';
 select distinct city
 from station
 where city rlike '^[^aeiou]';
+
+-- Weather Observation Station 10
+--------------------------------
+-- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+select distinct city
+from station
+where city rlike '[^aeiou]$';
