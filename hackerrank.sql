87ed277270393bdaf26078798a18c50f79b76a44
Natalie <49847911+natjng@users.noreply.github.com>
2020-01-05 18:00:54 -0800

    Matches name not starting with characters

--- hackerrank.sql
+++ hackerrank.sql
@@ -55,3 +55,11 @@ select distinct city
 from station
 where city rlike '^[aeiou]'
 and city rlike '[aeiou]$';
+
+-- Weather Observation Station 9
+--------------------------------
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+select distinct city
+from station
+where city rlike '^[^aeiou]';
